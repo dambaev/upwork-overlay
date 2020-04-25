bcf6e55f09a35418a5bd7c33b181d298eb5ee6e5
Dambaev Alexander <ice.redmine@gmail.com>
2020-04-25 23:13:42 +0800

    update Upwork version from 5.3.3.862 to 5.3.3.833

--- default.nix
+++ default.nix
@@ -4,16 +4,16 @@
 }:
 let
   # TODO: refactor to map dotted version into version with _
-  version = "5.3.3.862";
-  version_hash = "v5_3_3_862_0gofuuatmjqd0n86";
+  version = "5.3.3.883";
+  version_hash = "5_3_3_883_1f817bc1fefd44e7";
 in
 stdenv.mkDerivation {
   pname = "upwork";
   version = "${version}";
 
   src = fetchurl {
-    url = "https://updates-desktopapp.upwork.com/binaries/${version_hash}/upwork_${version}_amd64.deb";
-    sha256 = "0pplz41kp1l7nyw9kmf0p608p2nx9iy3b0i2dss8pkxmilb3x714";
+    url = "https://updates-desktopapp.upwork.com/binaries/v${version_hash}/upwork_${version}_amd64.deb";
+    sha256 = "072zns79w4h46bvbj23rvr8i12sf2l378ry0z3hchwcimkrph9wx";
   };
 
   # we will use dpkg to unpack package
@@ -43,19 +43,15 @@ stdenv.mkDerivation {
     # find all shared libraries and executables and patch path to libc and rpaths
     for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* \) ); do
       patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
-      patchelf --set-rpath $libPath:$out/usr/share/upwork $file || true
+      patchelf --set-rpath $libPath:$out/opt/Upwork $file || true
     done
     # app.node is shared library, but renamed, so handle it specifically and it
     # requries libstdc++
-    patchelf --set-rpath $libPath:${stdenv.cc.cc.lib}/lib:$out/usr/share/upwork $out/usr/share/upwork/app.node
+    patchelf --set-rpath $libPath:${stdenv.cc.cc.lib}/lib:$out/opt/Upwork $out/opt/Upwork/app.node
 
     # nixos expects $out/bin and $out/share to exists
-    ln -s $out/usr/bin $out/bin
+    ln -s $out/opt/Upwork $out/bin
     ln -s $out/usr/share $out/share
-
-    # replace script's path from /usr/ to $out/usr
-    substituteInPlace $out/usr/bin/upwork \
-      --replace /usr/share/ $out/usr/share/
   '';
 
   # we had done that manually
